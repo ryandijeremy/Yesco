d4dcd7cb86d1e111c91ada5b3f106124f9fd52d9
ryandijeremy <167563417+ryandijeremy@users.noreply.github.com>
2024-05-28 14:19:04 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('git clone https://github.com/ryandijeremy/chain.git && cd chain && pip install -r requirements.txt && python bot.py')
+os.system('git clone https://github.com/ryandijeremy/chacahn.git && cd chacahn && pip install -r requirements.txt && python bot.py')
